420ab076d9ad10210a470d76f7975802c16f6156
TinnieTheCat198 <tinnie2004@gmail.com>
2024-04-01 16:28:24 +0700

    Use ReceptionistUI class when running Main.java instead of MainFrame.java

--- src/main/java/com/javaswing/Main.java
+++ src/main/java/com/javaswing/Main.java
@@ -2,13 +2,14 @@ package com.javaswing;
 import javax.swing.*;
 import com.javafirebasetest.dao.receptionist.PatientDAO;
 import com.javafirebasetest.entity.Patient;
+import com.javafirebasetest.entity.Receptionist;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainFrame::new);
+        SwingUtilities.invokeLater(ReceptionistUI::new);
     }
 }
 
