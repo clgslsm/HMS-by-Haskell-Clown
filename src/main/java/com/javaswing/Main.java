f41ec25978ac3b725149b64b4f12c32bdc8c7a3a
SpyBurner <zombiepigman1505@gmail.com>
2024-04-05 21:12:19 +0700

    Medicine and Machine class toMap() changed, populate data

--- src/main/java/com/javaswing/Main.java
+++ src/main/java/com/javaswing/Main.java
@@ -1,25 +1,30 @@
 package com.javaswing;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.javafirebasetest.dao.DBManager;
 import com.javafirebasetest.dao.DoctorDAO;
 import com.javafirebasetest.dao.MedRecDAO;
 import com.javafirebasetest.entity.DeptType;
 import com.javafirebasetest.entity.Doctor;
+import com.javafirebasetest.entity.Machine;
+import com.javafirebasetest.entity.Medicine;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
-//        List<Doctor> doctorList = DoctorDAO.getDoctorByDepartment(DeptType.NUCLEAR_MEDICINE);
-//        for (Doctor doc : doctorList){
-//            System.out.println(doc);
-//        }
-
-        Doctor doctor = DoctorDAO.getDoctorById("EQTOqwQ55vGsqfC7YrPS");
-        System.out.println(doctor);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 //        SwingUtilities.invokeLater(ReceptionistUI::new);
     }
+
 }
 
 
