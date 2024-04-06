724ad7e7308814009423e02173391e19678c2d83
SpyBurner <zombiepigman1505@gmail.com>
2024-04-06 12:20:25 +0700

    relocate MachineDAO

--- src/main/java/com/javaswing/Main.java
+++ src/main/java/com/javaswing/Main.java
@@ -8,30 +8,30 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor newDoctor = new Doctor(
-                "lmao",
-                "Strange",
-                DeptType.PSYCHIATRY
-        );
-
-
-        DoctorDAO.addDoctor(newDoctor);
-
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        DoctorDAO.updateDoctor("lmao", "department", DeptType.DENTAL.getValue());
-
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        DoctorDAO.deleteDoctorById("lmao");
+//        Doctor newDoctor = new Doctor(
+//                "lmao",
+//                "Strange",
+//                DeptType.PSYCHIATRY
+//        );
+//
+//
+//        DoctorDAO.addDoctor(newDoctor);
+//
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        DoctorDAO.updateDoctor("lmao", "department", DeptType.DENTAL.getValue());
+//
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        DoctorDAO.deleteDoctorById("lmao");
     }
 }
 
