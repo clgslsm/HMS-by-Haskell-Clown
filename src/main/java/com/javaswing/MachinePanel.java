4bf8b56059206f464428fc90f6548f57a3c8ef5f
clgslsm <clgslsm@gmail.com>
2024-04-06 08:39:24 +0700

    Update login

--- src/main/java/com/javaswing/MachinePanel.java
+++ src/main/java/com/javaswing/MachinePanel.java
@@ -11,7 +11,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import com.javafirebasetest.dao.receptionist.PatientDAO;
+import com.javafirebasetest.dao.PatientDAO;
 class MachinePanel extends JPanel {
     ArrayList<Machine> data = new ArrayList<>();
     MachineDefaultPage defaultPage;
